3fa50573a1b5fa0f590c0ac25cac5c38963fb158
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-10-04 21:54:34 +0100

    initial folders

---
+++ src/sum.js
@@ -0,0 +1,12 @@
+
+class User {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+}
+
+export function sum(a,b) {
+    return a + b
+}
+
---
+++ src/sum.test.js
@@ -0,0 +1,14 @@
+import {sum} from "./sum"
+
+describe("example tests", ()=> {
+
+    it("should add 1 + 2 to eqaul 3", () => {
+        const result = sum(1,2);
+        expect(result).toBe(3);
+    })
+
+    it("object assigment", () => {
+        const obj = {};
+        expect(obj).toEqual({})
+    })
+})
