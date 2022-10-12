79ae1179a5e979f8aa8783b0e4a575be06ec72c7
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-10-12 17:28:29 +0100

    Updated style.css

--- src/script.js
+++ src/script.js
@@ -62,9 +62,9 @@ const blocks = [
 function addBlocks() {
     for (let i = 0; i < blocks.length; i++) {
         const block = document.createElement('div')
-        block.classList.add('block')
         block.style.left = blocks[i].bottomLeft[0] + 'px'
         block.style.bottom = blocks[i].bottomLeft[1] + 'px'
+        block.classList.add('block')
         grid.appendChild(block)
     }
 }
@@ -187,6 +187,8 @@ function startPlayingGame() {
     scoreDisplay.innerHTML = score
     document.addEventListener('keydown', moveUser)
     timerId = setInterval(moveBall, 30)
+   
 }
 
 startGame.addEventListener('click', startPlayingGame)
+
