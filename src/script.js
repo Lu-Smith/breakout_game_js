8f6df6a009f7a57749dab4eed187345dff5a7c64
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-10-13 10:53:55 +0100

    Added button animation

--- src/script.js
+++ src/script.js
@@ -205,3 +205,16 @@ function startPlayingGame() {
 
 startGame.addEventListener('click', startPlayingGame)
 
+
+startGame.addEventListener('mouseover', (e) => {
+    let x = e.clientX - e.target.offsetLeft
+    let y = e.clientY - e.target.offsetTop
+    let ripples = document.createElement('span')
+    ripples.style.left = x + 'px'
+    ripples.style.top = y + 'px'
+    start.appendChild(ripples)
+
+    setTimeout(() => {
+        ripples.remove()
+    }, 3000)
+} )
